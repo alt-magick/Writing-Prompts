2078308e0037be3b3b660c6121d954b7366d4742
Corey White <realityhacker@gmail.com>
2022-12-25 20:33:57 -0500

    Uploaded Christmas 2022

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+import sys
+def random_line(fname):
+    lines = open(fname).read().splitlines()
+    return random.choice(lines)
+file = sys.argv[1]
+prompt = int(sys.argv[2])
+print("")
+print("Prompts")
+print("--------")
+
+for x in range(prompt):
+    print(random_line(file))
+print("")
